5fd43c0a1973549bd726f551622a7c78e249054b
Ali-Chouchene <choucheneali9801@gmail.com>
2022-12-16 17:12:02 +0100

    ended

---
+++ script/script.js
@@ -0,0 +1,75 @@
+/*
+Consegna
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+ -CONSIGLI
+
+MILESTONE 1
+Prepariamo l'HTML ed il CSS per ottenere il risultato grafico che vediamo nell'immagine allegata.
+MILESTONE 2
+Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
+MILESTONE 3
+In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
+#MILESTONE 4
+Al click sulla cella, stampiamo il numero della cella cliccata in console, poi coloriamo la cella d'azzurro!
+
+BONUS
+
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+Note:
+- questo bonus richiederà una riflessione extra per quel che riguarda il calcolo della larghezza delle celle ;)
+Consigli del giorno:
+Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
+Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+Le validazioni e i controlli possiamo farli anche in un secondo momento.
+*/
+
+
+
+// ! FUNCTIONS
+const createCell = (number) => {
+    const cell = document.createElement("div");
+    if (number === "101") {
+        cell.classList.add("celleasy")
+    } else if (number === "82") {
+        cell.classList.add("cellmedium")
+    } else {
+        cell.classList.add("cellhard")
+    }
+    return cell;
+}
+
+
+// ! PRENDO ELEMENTI DAL DOM
+const grid = document.getElementById("grid");
+const btn = document.getElementById("button");
+const h1 = document.getElementById("h1");
+// !  AVVIO
+btn.addEventListener("click", function () {
+    //refresh grid
+    grid.innerHTML = "";
+
+    const userChoice = document.getElementById("select").value;
+    for (let i = 1; i < userChoice; i++) {
+        const cell = createCell(userChoice);
+
+        grid.appendChild(cell);
+        cell.innerText = (i);
+
+        cell.addEventListener("click", () => {
+            console.log(i);
+            cell.classList.toggle("clicked");
+        })
+    }
+    h1.classList.add("d-none");
+    grid.classList.remove("d-none");
+
+
+})
+
